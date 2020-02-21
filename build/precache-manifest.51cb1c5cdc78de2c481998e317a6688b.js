2a5835475072427cf52d056933a576a7256dd364
Andrey <erofteev@gmail.com>
2020-02-21 18:11:45 +0300

    added build config

---
+++ build/precache-manifest.51cb1c5cdc78de2c481998e317a6688b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c17b4f5170237ba1233a2e367953dfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c21ffe728d48727c02b7",
+    "url": "/static/css/main.3f198898.chunk.css"
+  },
+  {
+    "revision": "589b05e49c246909386d",
+    "url": "/static/js/2.fefe5ab8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.fefe5ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21ffe728d48727c02b7",
+    "url": "/static/js/main.e2486d53.chunk.js"
+  },
+  {
+    "revision": "844d94be027ecd06444f",
+    "url": "/static/js/runtime-main.5a5f5702.js"
+  },
+  {
+    "revision": "829f81ef4eeab6d3c03dfb09cd0c119f",
+    "url": "/static/media/green_tank.829f81ef.svg"
+  },
+  {
+    "revision": "256832b1f0645d94b725d82030750498",
+    "url": "/static/media/red_tank.256832b1.svg"
+  }
+]);
